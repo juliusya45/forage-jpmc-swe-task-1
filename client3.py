5e28215c09f6dc8af38797be866e844923cb1534
Julius Yang <68701101+juliusya45@users.noreply.github.com>
2023-07-11 13:35:28 -0700

    finished task 1 and completed assert tests

--- client3.py
+++ client3.py
@@ -35,14 +35,19 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    #stock price is bid_price + ask_price divided by 2
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    #to prevent dividing by 0 error
+    if (price_b == 0):
+        return
+    else:
+        return price_a/price_b
 
 
 # Main
@@ -52,8 +57,13 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        ##create a dictionary called prices to correlate stock name to price
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            #creating a key (stock name) value (price of stock) pair
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(price, price))
+        #utilizing the dictionary to get the prices of the two stocks
+        #and calling the getRatio funtion to output the ratio of the two prices
+        print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
